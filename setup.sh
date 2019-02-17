d0ddc39de5f3c62df4cdaa69caaf022d5df18ac0
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2019-02-17 15:55:04 +0900

    Improve builddep installation

--- setup.sh
+++ setup.sh
@@ -7,10 +7,6 @@ if [ -f "$DATA_DIR/${PACKAGE}_${VERSION_BASE}.orig.tar.gz" ]; then :; else
   exit 127
 fi
 
-sudo apt update
-sudo apt -y upgrade
-sudo apt -y install mpi-default-dev libboost-all-dev libhdf5-serial-dev libeigen3-dev doxygen graphviz
-
 rm -rf $BUILD_DIR
 set -x
 
@@ -19,3 +15,7 @@ cd $(dirname $BUILD_DIR)
 cp -p $DATA_DIR/${PACKAGE}_${VERSION_BASE}.orig.tar.gz .
 tar zxf ${PACKAGE}_${VERSION_BASE}.orig.tar.gz
 cp -frp $SCRIPT_DIR/debian $BUILD_DIR
+
+sudo apt update
+sudo apt -y upgrade
+dpkg-checkbuilddeps 2>&1 | sed 's/dpkg-checkbuilddeps: Unmet build dependencies: //' | xargs sudo apt -y install
